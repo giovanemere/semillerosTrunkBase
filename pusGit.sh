e3c56a8f850064e0f8a1c53f4dc2f28cc6d5ac1e
giovanemere <giovanemere@gmail.com>
2023-03-08 23:43:26 -0500

    add push git

--- pusGit.sh
+++ pusGit.sh
@@ -5,36 +5,64 @@ clear
 #username=$1
 #token=$2
 #repo=$3
-comment=$1
-rama=$2
+comment=${1}
+rama=${2}
+clon2=${3}
 
+# Validar estructura de Repositorio
+   if [[ -z $comment ]]; then
+      echo  '---------------------------------------------------------------------'
+      echo "Ejemplo de Ejecucion"
+      echo " sh pushGit.sh \"comentario\" \"rams\" \"clone\" "
+      echo  '---------------------------------------------------------------------'
+      echo -e "\--- Fin del Script -------------------------"
+      read -p "Press [Enter] key to continue..." readEnterKey
+      exit 1
+   fi
 
-git config --global user.email "giovanemere@gmail.com"
-git config --global user.name "giovanemere"
+# Configuracion gobal del repositorio
+   git config --global user.email "giovanemere@gmail.com"
+   git config --global user.name "giovanemere"
+   git config --global core.autocrlf false
 
+# Preguntar si desea Clonar
+   read -p ">> Digite Y/N si desea clonar : " clone
+   if [[ $clone =~ ^[Yy]$ ]]
+   then
+      git clone $clone
+   fi
+   
+# Pasos de Clonacion
+   #Imprimir Variables
+      echo -e "--------------------------------------------"  
+      echo "comentario :[$comment] | rama :[$rama]"
+      echo -e "--------------------------------------------"  
+   
+   # Status
+      echo "--------------------------------------------"   
+      echo "inicia carga en git"
+      echo "--------------------------------------------"   
+         git status
+         echo "- Reporte Status----------------------------"
+      read -p "Press [Enter] key to continue..." readEnterKey
 
-# Ejecucion
+   # Add todo slo scambios de la carpeta actual
+      echo "--------------------------------------------"   
+         git add .
+         echo "- Reporte Status----------------------------"
+      read -p "Press [Enter] key to continue..." readEnterKey
+   
+   # Agregar Comentario
+      echo "--------------------------------------------"   
+         git commit -m "$comment"
+         echo "- Agregar Comenatario ---------------------"
+      read -p "Press [Enter] key to continue..." readEnterKey
 
-   #    cd /home/giovanemere/Aprovisionamiento_Linux
-   #   git clone https://$username:$token@github.com/$repo
+   #Subir cambios a la rama remota
+      echo "--------------------------------------------"   
+         git push origin $rama
+         echo "- Subir a $rama --------------------------"
+      echo "--- Fin del Script -------------------------"
 
-   #if [[ $clone =~ ^[Yy]$ ]]
-   #then
-   #   git clone https://$username:$token@github.com/$repo
-   #fi
-    
-    echo "comment [$comment] | rama [$rama]"
-    echo "inicia carga en git"
-       
-    git status
-    read -p "Press [Enter] key to continue..." readEnterKey
-
-    git add .
-    read -p "Press [Enter] key to continue..." readEnterKey
-    
-    git commit -m "$comment"
-    read -p "Press [Enter] key to continue..." readEnterKey
-
-    git push origin $rama
-    echo -e "\n----- Fin del Script -----------------------------------------------------------"
-    read -p "Press [Enter] key to continue..." readEnterKey
+read -p "Press [Enter] key to continue..." readEnterKey
+#Fin Script
